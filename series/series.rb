c184f119f1da0b704f7961a2dcfc2c2b9582ebcf
Eli Ashby <eli.ashby@mx.com>
2020-04-21 16:20:03 -0600

    adding series solution

--- series/series.rb
+++ series/series.rb
@@ -1,9 +1,11 @@
 class Series
   def initialize(series_string)
-    @series_string = series_string
+    @series_string = series_string.chars
   end
 
   def slices(split_by)
-    @series_string.partition { |n| n.split_by }
+    raise ArgumentError unless split_by <= @series_string.length
+
+    @series_string.each_cons(split_by).map(&:join)
   end
 end
